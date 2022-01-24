0fcbbde5459fde0e44d34ae6e80a8129dc488399
Blue <purgekingsubaru@gmail.com]>
2022-01-24 18:14:25 +0530

    Initial commit

---
+++ sayaka2.py
@@ -0,0 +1,13 @@
+var1="sayaka"
+var2=83
+var3=49.23
+var4 ="maizono"
+print(var1)
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(var2 + var3)
+print(var1 + var4)
+print(str(var2) + str(var3))
+print(10*(var1 + var4  + "\n"))
+print(10*(str(var2+var3)+"\n"))
